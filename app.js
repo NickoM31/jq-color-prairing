602a752315a037b9a0e5fe5e17b6595f9fb53c31
Nicko <nicolasmathieu31@hotmail.com>
2016-10-07 09:49:54 +0200

    color prairing

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// good luck !
+
+
+$("input").css("background",function(){
+
+	return $(this).data("color");
+});
+
+$("input").on("click",function(){
+
+	var yep = $(this).data("color")
+
+	if (checked()){
+		$(".main").css("color",yep);
+	}
+	else{$("body").css("background-color",yep)
+}
+
+});
+
+function checked(){
+	return $("#modify-texte").is(":checked");
+
+}
+
+checked();
+
+
+
