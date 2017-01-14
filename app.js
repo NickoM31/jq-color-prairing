dd1ffed3d8200c8bae4c52a6b483ba4f42e153fc
Nicko <nicolasmathieu31@hotmail.com>
2017-01-14 13:55:52 +0100

    Update js janvier 2017

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-
-
 $("input").css("background",function(){
 
 	return $(this).data("color");
 });
 
 $("input").on("click",function(){
-
 	var couleur = $(this).data("color")
-
 	if (checked()){
 		$(".main").css("color",couleur);
-
 	}else{$("body").css("background-color",couleur)
 }
-
 });
-
 function checked(){
 	return $("#modify-texte").is(":checked");
-
 }
-
 checked();
 
 
